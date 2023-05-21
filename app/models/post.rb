6ceade7ca60683dcded08a71d1a08c473c6b5103
Daniel Rice <danielrice5627@gmail.com>
2023-05-21 16:30:14 -0700

    commit tree dcb5eed2766674fc26f8776603332fa44c080cce

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validates :title, presence: true
+    validate :is_clickbait
+
+    def is_clickbait
+       if title == "You Won't Believe These True Facts"
+       else
+        click_bait_error
+       end
+    end
+
+    def click_bait_error
+        errors.add(:title, "Not Clicky ENough!!")
+    end
 end
